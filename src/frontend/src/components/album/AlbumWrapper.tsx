9c8a59a3a0235348a7bc26ab9d7826641aa036d8
albertchriss <19623210@std.stei.itb.ac.id>
2024-12-16 23:47:56 +0700

    fix: album wrapper

--- src/frontend/src/components/album/AlbumWrapper.tsx
+++ src/frontend/src/components/album/AlbumWrapper.tsx
@@ -11,7 +11,8 @@ export const AlbumWrapper = ({ imgSrc, title, score }: AlbumWrapperProps) => {
     <div className="bg-white rounded-2xl px-4 pt-4 pb-8 w-fit h-fit shadow-lg hover:scale-110 hover:cursor-pointer transition-all duration-300">
       <AlbumCard imgSrc={imgSrc} title={title} className="shadow-none" />
       <div className="flex flex-col space-y-2 mt-4">
-        <p className="text-sm text-gray-500">Similarity: {score?.toFixed(2)}%</p>
+        {score && <p className="text-sm text-gray-500">Similarity: {score?.toFixed(2)}%</p>}
+        
       </div>
     </div>
   );
