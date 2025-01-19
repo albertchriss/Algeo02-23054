21ed7d8fa350b55fe6ed3e9b5d5971cf7bdcd277
albertchriss <19623210@std.stei.itb.ac.id>
2025-01-19 20:23:37 +0700

    fix: adding suspense on useSearchParams

--- src/frontend/src/app/album/page.tsx
+++ src/frontend/src/app/album/page.tsx
@@ -1,13 +1,15 @@
 import { AlbumList } from "@/components/album/AlbumList";
 import { SearchBar } from "@/components/search/SearchBar";
-import React from "react";
+import React, { Suspense } from "react";
 
 const AlbumPage = () => {
   return (
     <div className="py-10 px-20 flex flex-col space-y-8">
       <SearchBar currentUrl="/album" />
       <h1 className="text-5xl font-bold">Albums</h1>
-      <AlbumList />
+      <Suspense fallback={<div className="w-full h-auto aspect-video flex justify-center items-center text-gray-500 italic">Loading...</div>}>
+        <AlbumList />
+      </Suspense>
     </div>
   );
 };
--- src/frontend/src/app/song/page.tsx
+++ src/frontend/src/app/song/page.tsx
@@ -1,13 +1,15 @@
 import { SearchBar } from "@/components/search/SearchBar";
 import { SongList } from "@/components/song/SongList";
-import React from "react";
+import React, { Suspense } from "react";
 
 const AlbumPage = () => {
   return (
     <div className="p-10 px-20 flex flex-col space-y-8">
       <SearchBar currentUrl="/song" />
       <h1 className="text-5xl font-bold">Songs</h1>
-      <SongList />
+      <Suspense fallback={<div className="w-full h-auto aspect-video flex justify-center items-center text-gray-500 italic">Loading...</div>}>
+        <SongList />
+      </Suspense>
     </div>
   );
 };
